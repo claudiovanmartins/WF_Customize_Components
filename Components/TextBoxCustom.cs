2b5396b885922f5b5190067a0e13d8afeddc4919
Claudiovan Martins do Nascimento <claudiovan.martins@hotmail.com>
2017-02-05 20:17:46 -0200

    First Commit

---
+++ Components/TextBoxCustom.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Components
+{
+    public class TextBoxCustom : TextBox
+    {
+        private Color _backColorIn;
+
+        [Category("Configurações de Cores"),
+        Description("Cor de Fundo ao receber foco."),
+        RefreshProperties(RefreshProperties.Repaint)]
+        public Color BackColorIn
+        {
+            get
+            {
+                return _backColorIn;
+            }
+
+            set
+            {
+                _backColorIn = value;
+            }
+        }
+
+        private Color _backColorOut;
+
+        [Category("Configurações de Cores"),
+        Description("Cor de Fundo ao perder foco."),
+        RefreshProperties(RefreshProperties.Repaint)]
+        public Color BackColorOut
+        {
+            get
+            {
+                return _backColorOut;
+            }
+
+            set
+            {
+                _backColorOut = value;
+            }
+        }
+
+        private Color _foreColorIn;
+        [Category("Configurações de Cores"),
+        Description("Cor da letra ao receber foco."),
+        RefreshProperties(RefreshProperties.Repaint)]
+        public Color ForeColorIn
+        {
+            get { return _foreColorIn; }
+            set { _foreColorIn = value; }
+        }
+
+        private Color _foreColorOut;
+        [Category("Configurações de Cores"),
+        Description("Cor da letra ao perder foco."),
+        RefreshProperties(RefreshProperties.Repaint)]
+        public Color ForeColorOut
+        {
+            get { return _foreColorOut; }
+            set { _foreColorOut = value; }
+        }
+
+        public TextBoxCustom()
+        {
+            _backColorIn = Color.Silver;
+            _backColorOut = Color.White;
+
+            _foreColorIn = Color.Black;
+            _foreColorOut = Color.Red;
+        }
+
+        protected override void OnEnter(EventArgs e)
+        {
+            base.OnEnter(e);
+            BackColor = _backColorIn;
+            ForeColor = _foreColorIn;    
+        }
+
+        protected override void OnLeave(EventArgs e)
+        {
+            base.OnLeave(e);
+            BackColor = _backColorOut;
+            ForeColor = _foreColorOut; 
+        }
+    }
+}
---
+++ WF_Components_Example/Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace WF_Components_Example
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxCustom1 = new Components.TextBoxCustom();
+            this.textBoxCustom2 = new Components.TextBoxCustom();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(19, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(166, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Testando o TextBox Customizado";
+            // 
+            // textBoxCustom1
+            // 
+            this.textBoxCustom1.BackColorIn = System.Drawing.Color.Silver;
+            this.textBoxCustom1.BackColorOut = System.Drawing.Color.White;
+            this.textBoxCustom1.ForeColorIn = System.Drawing.Color.Black;
+            this.textBoxCustom1.ForeColorOut = System.Drawing.Color.Red;
+            this.textBoxCustom1.Location = new System.Drawing.Point(22, 64);
+            this.textBoxCustom1.Name = "textBoxCustom1";
+            this.textBoxCustom1.Size = new System.Drawing.Size(236, 20);
+            this.textBoxCustom1.TabIndex = 0;
+            // 
+            // textBoxCustom2
+            // 
+            this.textBoxCustom2.BackColorIn = System.Drawing.Color.Silver;
+            this.textBoxCustom2.BackColorOut = System.Drawing.Color.White;
+            this.textBoxCustom2.ForeColorIn = System.Drawing.Color.Black;
+            this.textBoxCustom2.ForeColorOut = System.Drawing.Color.Red;
+            this.textBoxCustom2.Location = new System.Drawing.Point(22, 100);
+            this.textBoxCustom2.Name = "textBoxCustom2";
+            this.textBoxCustom2.Size = new System.Drawing.Size(236, 20);
+            this.textBoxCustom2.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(19, 48);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(119, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "[Após digitar, tecle Tab]";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 154);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBoxCustom2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxCustom1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Components.TextBoxCustom textBoxCustom1;
+        private System.Windows.Forms.Label label1;
+        private Components.TextBoxCustom textBoxCustom2;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
